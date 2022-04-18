027f1b98c8a7df3be42e4e6d3676830f0a9728f1
Eguchi Yoshihiro <eguchi@EguchinoMacBook-Pro.local>
2022-04-18 19:27:22 +0900

    first tanpopo

---
+++ cms/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from cms.models import Book, Impression
+
+
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ('name', 'publisher', 'page', )
+        
+class ImpressionForm(ModelForm):
+    class Meta:
+        model = Impression
+        fields = ('comment', )
---
+++ cms/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from cms import views
+
+
+app_name = 'cms'
+urlpatterns = [
+    path('book/', views.book_list, name='book_list'),
+    path('book/add/', views.book_edit, name='book_add'),
+    path('book/mod/<int:book_id>/', views.book_edit, name='book_mod'),
+    path('book/del/<int:book_id>/', views.book_del, name='book_del'),
+    path('impression/<int:book_id>/', views.ImpressionList.as_view(), name='impression_list'),
+    path('impression/add/<int:book_id>/', views.impression_edit, name='impression_add'),
+    path('impression/mod/<int:book_id>/<int:impression_id>/', views.impression_edit, name='impression_mod'),
+    path('impression/del/<int:book_id>/<int:impression_id>/', views.impression_del, name='impression_del'),
+]
