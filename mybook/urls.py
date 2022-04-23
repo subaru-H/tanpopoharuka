000f457ae3e8c250de5c63a7189e86b16a11c97a
muhi4 <rwymzl@gmail.com>
2022-04-23 13:27:15 +0900

    トップページのurl修正

--- mybook/urls.py
+++ mybook/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('cms/', include('cms.urls')),
     path('admin/', admin.site.urls),
     path('', include('cms.urls')),
 ]
