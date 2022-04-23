49c73118bd8296e36c9b9a8fd28042160d29fb1a
Eguchi Yoshihiro <eguchi@EguchinoMacBook-Pro.local>
2022-04-23 16:23:59 +0900

    ログアウトバグは残ってる

--- cms/urls.py
+++ cms/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'cms'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index_page'),
+    path('tobook/', views.IndexView2.as_view(), name='to_book'),
     path('book/', views.book_list, name='book_list'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
